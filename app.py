e896edfe32540305779dfdb4b278a1da303f945d
Mehmet Taskiner <mehmet.taskiner@vngrs.com>
2018-04-02 18:11:33 +0300

    Add sample application

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import os
+import flask
+import MySQLdb
+
+application = flask.Flask(__name__)
+application.debug = True
+
+@application.route('/hello')
+def index():
+    storage = Storage()
+    storage.populate()
+    return 'hello ' + str(storage.pageview())
+
+
+class Storage():
+  def __init__(self):
+    self.db = MySQLdb.connect(
+      user   = os.getenv('MYSQL_USERNAME'),
+      passwd = os.getenv('MYSQL_PASSWORD'),
+      db     = os.getenv('MYSQL_INSTANCE_NAME'),
+      host   = os.getenv('MYSQL_SERVICE_HOST'),
+      port   = int(os.getenv('MYSQL_SERVICE_PORT'))
+    )
+
+    cur = self.db.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS pageviews(pageview INT)")
+
+  def populate(self):
+    cur = self.db.cursor()
+    cur.execute("INSERT INTO pageviews(pageview) VALUES(1234)")
+    self.db.commit()
+
+  def pageview(self):
+    cur = self.db.cursor()
+    cur.execute("SELECT count(*) FROM pageviews")
+    row = cur.fetchone()
+    return row[0]
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=3000)
+
+
